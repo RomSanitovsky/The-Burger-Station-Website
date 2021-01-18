f0f582d96db91d6dcd2e15dcff385e7e66e201e1
Or Levi <levior9@gmail.com>
2021-01-18 20:27:51 +0200

    Added bootstap

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import BacktoTop from './components/backtop';
+import Booktable from './components/booktable';
+import Chefs from './components/chefs';
+import Contact from './components/contact';
+import Events from './components/events';
+import Footer from './components/footer';
+import Gallery from './components/gallery';
+import Header from './components/header';
+import Hero from './components/hero';
+import Menu from './components/menu';
+import Specials from './components/specials';
+import Testimonials from './components/testimonials';
+import Topbar from './components/topbar';
+import Whyus from './components/whyus';
+import {BrowserRouter, Link, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Topbar />
+      <Header />
+      <Hero />
+      <main id="main">
+        <About />
+        <Whyus />
+        <Menu />
+        <Specials />
+        <Events />
+        <Booktable />
+        <Testimonials />
+        <Gallery />
+        <Chefs />
+        <Contact />
+      </main>
+      <Footer />
+      <BacktoTop />
+
+    </BrowserRouter>
   );
 }
 
