7d4e49370d434d900a5bdc96b21ca0be501dacc8
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-03-01 17:48:09 +0200

    multiple changes

--- client/front/src/components/authentication/sign-up.js
+++ client/front/src/components/authentication/sign-up.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function SignUp() {
     const [fullName, setFullName] = useState({
         fName: "",
         lName: ""
     });
+
+    const [items, setItems] = useState([]);
+
+    useEffect(async () => {
+        if (items.length < 1) {
+            const response = await fetch("http://localhost:8000/api/items");
+            const data = await response.json();
+            setItems(data.data.data)
+        }
+        else { console.log("else"); console.log(items) }
+    }, [items])
+
+
+    const [branches, setBranches] = useState([]);
+
+    useEffect(async () => {
+        if (branches.length < 1) {
+            const response = await fetch("http://localhost:8000/api/branches");
+            const data = await response.json();
+            setBranches(data.data.data)
+            console.log(branches)
+        }
+        else { console.log("else"); console.log(branches) }
+    }, [branches])
+
     //handle the change
     function handleChange(event) {
         const { value, name } = event.target;
@@ -26,6 +54,7 @@ function SignUp() {
         });
     }
     return (
+
         <div id="book-a-table">
             <section id="book-a-table" className="book-a-table">
                 <div className="container" data-aos="fade-up">
@@ -33,6 +62,7 @@ function SignUp() {
                         <h2>Sign Up</h2>
                         <p>Come to the tasty side {fullName.fName}</p>
                     </div>
+
                     <form action="forms/book-a-table.php" className="php-email-form" >
                         <div className="form-row">
                             <div className="col-lg-12 col-md-12 form-group">
@@ -55,14 +85,20 @@ function SignUp() {
                                 <div className="section-title">
                                     <h2>Choose Your Favorite Burger</h2>
                                 </div>
-                                <select name="fav-item" className="form-control" id="fav-item" placeholder="Choose Your Favorite Burger" data-rule="minlen:4" autocomplete="off" />
+                                <select name="fav-item" className="form-control" id="fav-item" placeholder="Choose Your Favorite Burger" data-rule="minlen:4" autocomplete="off">
+                                    <option> {" "}</option>
+                                    {items.map(item => { if (item.name.includes("Burger") || item.name.includes("burger")) { return <option>{item.name}</option> } })}
+                                </select>
                                 <div className="validate" />
                             </div>
                             <div className="col-lg-12 col-md-12 form-group">
                                 <div className="section-title">
                                     <h2>Choose Your Favorite Branch</h2>
                                 </div>
-                                <select name="fav-branch" className="form-control" id="fav-branch" placeholder="Choose Your Favorite Branch" data-rule="minlen:4" autocomplete="off" />
+                                <select name="fav-branch" className="form-control" id="fav-branch" placeholder="Choose Your Favorite Branch" data-rule="minlen:4" autocomplete="off" >
+                                    <option> {" "}</option>
+                                    {branches.map(item => <option>{item.address} {item.city} {item.district}</option>)}
+                                </select>
                                 <div className="validate" />
                             </div>
                         </div>
