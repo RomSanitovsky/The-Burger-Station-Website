0ee9e068fc4351bfe9cd092421f5e8e07e3d8903
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-12 12:24:36 +0200

    scraping

--- server/imports/usersGenerator.js
+++ server/imports/usersGenerator.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const user = {};
 const userArray = [];
 
-for (let i = 0; i < 100; i += 1) {
+for (let i = 0; i < 200; i += 1) {
   user.email = faker.internet.email();
   user.password = faker.internet.password();
   user.passwordConfirm = user.password;
